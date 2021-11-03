926d72d49d3adede9f53e390ad8ab39a8e23796c
anallely753 <anallely.mtzc@comunidad.unam.mx>
2021-11-02 22:51:48 -0600

    para clase del 03 de nov

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminUserController;
+use App\Http\Controllers\AdminProductoController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('admin');
+Route::get('vendedor/home', [HomeController::class, 'vendedorHome'])->name('vendedor.home')->middleware('vendedor');
+
+Route::resource('adminuser', AdminUserController::class);
+Route::resource('adminproducto', AdminProductoController::class);
+
+
